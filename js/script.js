67b4f658f0d1526dc8b7ea054162b56ea77c6615
diamant1991 <diamant1988@mail.ru>
2015-08-03 18:26:32 +0300

    first giles

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// Select
+$('.slct').click(function(){
+	var dropBlock = $(this).parent().find('.drop');
+
+	if( dropBlock.is(':hidden') ) {
+		dropBlock.slideDown(150);
+
+		$(this).addClass('active');
+		$('.drop').find('li').click(function(){
+			var selectResult = $(this).html();
+			$(this).parent().parent().find('input').val(selectResult);
+			$(this).parent().parent().find('.slct').removeClass('active').html(selectResult);
+
+			dropBlock.slideUp(150);
+		});
+	
+	} else {
+		$(this).removeClass('active');
+		dropBlock.slideUp(150);
+	}
+
+	return false;
+});
+
+$(document).mouseup(function (e) {
+    var container = $(".drop");
+    if (container.has(e.target).length === 0){
+        container.slideUp(150);
+        $('.slct').removeClass('active');
+    }
+});
+
+$(document).ready(function() {
+
+    $('.header').hover(function() {
+    	$('.sub-menu').slideDown(350)
+    }, function() {
+    	$('.sub-menu').slideUp(350)
+    });
+});
